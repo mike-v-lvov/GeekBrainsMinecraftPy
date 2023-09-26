d6ee73d087594dadcdf6a7d7c56156399c2af6a3
Mike V Lvov <Mike.V.Lvov@Gmail.com>
2023-09-26 19:46:21 +0300

    арена

--- arena.py
+++ arena.py
@@ -4,17 +4,33 @@ from mcpi.minecraft import Minecraft
 import time
 mc = Minecraft.create()
 
-class object():
+class Object():
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
 
-class Arena(Obgect):
+class Arena(Object):
     def __init__(self, x, y, z):
-        Obgect.__init__(self, x, y, z)
-        self.wigth = 10
+        Object.__init__(self, x, y, z)
+        self.width = 10
         self.height = 3
-        self.lenght = 20
-        self.finish = self.z + self.lenght - 1
+        self.length = 20
+        self.finish = self.z + self.length - 1
+        mc.setBlocks(self.x - 1, self.y, self.x - 1,
+                self.x + self.width + 1, self.y - 3, self.z + self.length + 1, 2)
+
+        mc.setBlocks(self.x - 1, self.y + 1, self.z - 1,
+                self.x + self.width + 1, self.y + self.height, self.z + self.length + 1, 20)
+
+        mc.setBlocks(self.x, self.y + 1, self.z,
+                self.x + self.width, self.y + self.finish, 41)
+
+        mc.setBlocks(self.x, self.y, self.finish,
+                self.x + self.width, self.y, self.finish, 41)
+
+
+x, y, z = mc.player.getTilePos()
+arena = Arena(x, y, z)
+
 
